b87e5d7c0ec3792cd794a4f5f739ba7a11729ccc
Lord RRG <ranveer.gokulgandhi@hotmail.com>
2025-06-22 14:04:01 +0530

    Project initialized, HTML and CSS with static arrays that contain symbols.

---
+++ charset.js
@@ -0,0 +1,7 @@
+// Characters from here may be used
+
+export const LOWERCASE = "abcdefghijklmnopqrstuvwxyz".split("");
+export const UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+export const NUMBERS = "0123456789".split("");
+export const SYMBOLS = "!@#$%^&*()_+-=[]{}|;:,.<>?/".split("");
+export const EXTENDED_SYMBOLS = "§©®¥π∞∆".split("");
---
+++ script.js
@@ -0,0 +1,5 @@
+// The JS code for generating random passwords
+
+function makePassword() {
+    // Based on user preferences, create a pool of characters from which characters can be pulled.
+}
