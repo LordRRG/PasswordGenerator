97fe0307dae394f7425dc8457cfd534faf2202c5
Lord RRG <ranveer.gokulgandhi@hotmail.com>
2025-06-22 14:14:50 +0530

    Client side and server side validation complete!

--- script.js
+++ script.js
@@ -3,3 +3,17 @@
 function makePassword() {
     // Based on user preferences, create a pool of characters from which characters can be pulled.
 }
+
+function validate() {
+    const useSymbols = document.getElementById("symbols").checked;
+    const useNumbers = document.getElementById("numbers").checked;
+    const useUppercase = document.getElementById("capital").checked;
+    const useLowercase = document.getElementById("lowercase").checked;
+    if (!useSymbols && !useNumbers && !useUppercase && !useLowercase) {
+        alert("Please select at least one character type (symbol, number, uppercase, or lowercase) to generate a password.");
+        console.log(useSymbols, useNumbers, useUppercase, useLowercase);
+        return false; // ❌ Exit the function early
+    } else {
+        makePassword();
+    }
+}
